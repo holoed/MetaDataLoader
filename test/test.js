a44721b94f3cd5d60556c70a98d90b29d3e00185
holoed@users.noreply.github.com <edmondo@gmail.com>
2015-06-30 00:01:39 +0100

    Add more tests

--- test/test.js
+++ test/test.js
@@ -6,15 +6,38 @@ var testUrl = "http://192.168.0.24/MyMoviesCatalog.json";
 var testMyList = {
     movies: [
     {
-      source: "http://192.168.0.24/Movies/The%20Usual%20Suspects.mp4",
-      title: "The Usual Suspects",
-      year: "1995"
+      source: "http://localhost/BackToTheFuture.mp4",
+      title: "Back to the future",
+      year: "1985"
+    },
+    {
+      source: "http://localhost/TheBourneSupremacy.mp4",
+      title: "The Bourne Supremacy",
+      year: "2004"
     }],
-    tvshows: []
+    tvshows: [
+      { title: "Columbo", year: "1971", seasons: [{
+        season: "Season 2",
+        episodes: [{
+            series: "Columbo",
+            season: "2",
+            episode: "3",
+            source: "http://localhost/Columbo/TheMostCrucialGame.mp4"
+          }]
+      }]},
+      { title: "Star Trek The Next Generation", year: "1987", seasons: [{
+        season: "Season 7",
+        episodes: [{
+            series: "Star Trek The Next Generation",
+            season: "7",
+            episode: "3",
+            source: "http://localhost/STTNG/Interface.mp4"
+          }]
+      }]}
+    ]
 };
 
 mock('../output/HttpClient', { fetch: function(url) {
-        console.log(url);
         return function(cb) {
           if (url == testUrl)
             return cb(testMyList);
@@ -31,38 +54,32 @@ mock('../output/HttpClient', { fetch: function(url) {
           if (url == "http://www.omdbapi.com/?t=Columbo&y=1971&plot=full&type=series&r=json")
             return cb({
               title: 'Columbo (The Series)',
-              year: '1971',
-              season: []
+              year: '1971'
             });
           if (url == "http://www.omdbapi.com/?t=Columbo&Season=2&Episode=3&plot=full&type=series&r=json")
             return cb({
-              title: "The most cruicial game",
+              title: "The most crucial game",
               released: "05 Nov 1972",
               season: "2",
               episode: "3"
             });
+            if (url == "http://www.omdbapi.com/?t=Star+Trek+The+Next+Generation&y=1987&plot=full&type=series&r=json")
+              return cb({
+                title: 'Star Trek The Next Generation (ST:TNG)',
+                year: '1987'
+              });
+            if (url == "http://www.omdbapi.com/?t=Star+Trek+The+Next+Generation&Season=7&Episode=3&plot=full&type=series&r=json")
+              return cb({
+                title: "Interface",
+                released: "02 Oct 1993",
+                season: "7",
+                episode: "3"
+              });
       }
   }
 });
 
 var loader = require ("MetadataLoader");
-
-// loader.fetchMovie ({ title: "Back to the future", year: "1985"}) (function (x) { console.log(x); }) ()
-
-// loader.fetchTVShow ({ title: "Columbo", year: "1971"}) (function (x) { console.log(x); }) ()
-
-
-
-// loader.fetchMovies (function (x) { console.log(x); }) ()
-
-// loader.fetchMoviesDetails (function (x) { console.log(x); })
-
-// loader.fetchTVShowsDetails (function (x) { console.log(x); })
-
-// loader.fetchTVShowEpisode ({ series:"Columbo", season:"1", episode:"1", source:"http://url/file.mp4" })  (function (x) { console.log(x); })
-
-//loader.getState ("http://192.168.0.24/MyMoviesCatalog.json") (function (x) { console.log(x.tvshows[1].seasons[0].episodes[2]); })
-
 
 describe ('MetadataLoader tests', function(){
 
@@ -95,8 +112,7 @@ describe ('MetadataLoader tests', function(){
       });
       assert.deepEqual({
         title: "Columbo (The Series)",
-        year: "1971",
-        season: []
+        year: "1971"
       }, result);
     })
 
@@ -109,7 +125,7 @@ describe ('MetadataLoader tests', function(){
         source: "http://localhost/Columbo/TheMostCrucialGame.mp4"
       })(function (x){ result = x; });
       assert.deepEqual({
-        title: "The most cruicial game",
+        title: "The most crucial game",
         released: "05 Nov 1972",
         season: "2",
         episode: "3",
@@ -134,6 +150,120 @@ describe ('MetadataLoader tests', function(){
         year: "2004",
         source: "http://localhost/TheBourneSupremacy.mp4"
       }, result[1]);
+    })
+
+    it ('should fetch tv shows details', function(){
+      var result = {};
+      loader.fetchTVShowsSeasonsDetails({
+          seasons: [{season: "Season 2", episodes: [{
+              series: "Columbo",
+              season: "2",
+              episode: "3",
+              source: "http://localhost/Columbo/TheMostCrucialGame.mp4"
+            }]
+          }]
+        })(function (x) { result = x; })
+        assert.deepEqual([
+          { season: "Season 2", episodes: [{
+              title: "The most crucial game",
+              season: "2",
+              episode: "3",
+              released: "05 Nov 1972",
+              source: "http://localhost/Columbo/TheMostCrucialGame.mp4"
+            }]
+          }
+        ], result);
+    })
+
+    it ('should fetch tv shows details', function(){
+      var result = {};
+      loader.fetchTVShowsDetails([
+        { title: "Columbo", year: "1971", seasons: [{
+          season: "Season 2",
+          episodes: [{
+              series: "Columbo",
+              season: "2",
+              episode: "3",
+              source: "http://localhost/Columbo/TheMostCrucialGame.mp4"
+            }]
+        }]},
+        { title: "Star Trek The Next Generation", year: "1987", seasons: [{
+          season: "Season 7",
+          episodes: [{
+              series: "Star Trek The Next Generation",
+              season: "7",
+              episode: "3",
+              source: "http://localhost/STTNG/Interface.mp4"
+            }]
+        }]}
+      ])(function(x) { result = x; })
+      assert.equal(2, result.length);
+      assert.deepEqual([
+        {
+          title:"Columbo (The Series)",
+          year:"1971",
+          seasons:[
+            { season:"Season 2",
+              episodes:[
+                { title:"The most crucial game",
+                 released:"05 Nov 1972",
+                 season:"2",
+                 episode:"3",
+                 source:"http://localhost/Columbo/TheMostCrucialGame.mp4"
+                }
+              ]
+            }
+          ]
+        },
+        { title:"Star Trek The Next Generation (ST:TNG)",
+          year:"1987",
+          seasons:[
+            { season:"Season 7",
+              episodes:[
+                { title:"Interface",
+                  released:"02 Oct 1993",
+                  season:"7",
+                  episode:"3",
+                  source:"http://localhost/STTNG/Interface.mp4"
+                }
+              ]
+            }
+          ]
+        }], result);
+    })
+
+    it ('should get state', function(){
+      var result = {};
+      loader.getState(testUrl)(function(x){ result = x; })
+
+      assert.deepEqual({
+        movies: [
+          { title:"Back to the future (The Movie)",year:"1985",source:"http://localhost/BackToTheFuture.mp4"},
+          { title:"The Bourne Supremacy (The Movie)",year:"2004",source:"http://localhost/TheBourneSupremacy.mp4"}],
+        tvshows:[
+        { title: 'Columbo (The Series)',
+          year: '1971',
+          seasons: [ { season: 'Season 2',
+                       episodes: [{
+                         title: "The most crucial game",
+                         released: "05 Nov 1972",
+                         season: "2",
+                         episode: "3",
+                         source: "http://localhost/Columbo/TheMostCrucialGame.mp4"
+                       }] }]
+        },
+        { title: 'Star Trek The Next Generation (ST:TNG)',
+          year: '1987',
+          seasons: [ { season: 'Season 7',
+                       episodes: [{
+                         title: "Interface",
+                         released: "02 Oct 1993",
+                         season: "7",
+                         episode: "3",
+                         source:"http://localhost/STTNG/Interface.mp4"
+                       }] }]
+        }]
+      }, result);
     })
 
 })
